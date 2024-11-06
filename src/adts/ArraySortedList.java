7b1425216712032fc1def7d0a37c6b1552c82bda
sner-s <samantha.essner@gmail.com>
2024-11-06 14:16:04 -0500

    Initial commit

---
+++ src/adts/ArraySortedList.java
@@ -0,0 +1,58 @@
+package adts;
+
+import interfaces.ListInterface;
+
+import java.util.Iterator;
+
+public class ArraySortedList<E> implements ListInterface<E>, Iterable<E>{
+    protected E[] list;               // array to hold this lists elements
+
+    protected int numElements = 0;    // number of elements in this list
+
+    protected final int DEFAULT_CAPACITY = 10;  // default capacity
+    protected int initialCapacity;              // original capacity
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public boolean remove(E element) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public E get(E element) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    private enum IteratorType {
+        ORDERED, REVERSE, RANDOM
+    }
+}
