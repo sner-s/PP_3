42f73de3319060fb327e194f7859a3e19a70d578
sner-s <samantha.essner@gmail.com>
2024-11-12 20:51:50 -0500

    Update DoublyLinkedIterator.java

--- src/iterators/DoublyLinkedIterator.java
+++ src/iterators/DoublyLinkedIterator.java
@@ -7,21 +7,64 @@ import java.util.Iterator;
 public class DoublyLinkedIterator<E> implements Iterator<E> {
 
 	private DLLNode<E> current;
+	private IteratorType iterationType;
+	private List<E> randomList;
+	private int index;
 
-	public DoublyLinkedIterator(DLLNode<E> head) {
-		this.current = head;
+	public DoublyLinkedIterator(DLLNode<E> head, DLLNode<E> tail, IteratorType iterationType) {
+		this.iterationType = iterationType;
+
+		switch (iterationType) {
+			case ORDERED:
+				this.current = head;
+				break;
+			case REVERSE:
+				this.current = tail;
+				break;
+			case RANDOM:
+				// Gather all elements in a list and shuffle for random iteration
+				randomList = new ArrayList<>();
+				DLLNode<E> node = head;
+				while (node != null) {
+					randomList.add(node.getData());
+					node = node.getNext();
+				}
+				Collections.shuffle(randomList);
+				index = 0;
+				break;
+		}
 	}
 
 	@Override
 	public boolean hasNext() {
-		return current != null;
+		switch (iterationType) {
+			case RANDOM:
+				return index < randomList.size();
+			default:
+				return current != null;
+		}
 	}
 
 	@Override
 	public E next() {
-		E element = current.getData();
-		current = current.getNext();
-		return element;
+		switch (iterationType) {
+			case ORDERED:
+				E orderedData = current.getData();
+				current = current.getNext();
+				return orderedData;
+			case REVERSE:
+				E reverseData = current.getData();
+				current = current.getPrev();
+				return reverseData;
+			case RANDOM:
+				return randomList.get(index++);
+			default:
+				throw new IllegalStateException("Invalid iteration type");
+		}
+	}
+
+	public enum IteratorType {
+		ORDERED, REVERSE, RANDOM
 	}
 
 }
